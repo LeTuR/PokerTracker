2ac035742b6e21e2d63f4d9186757c220c385b68
CESARE HERRIAU <arthur.cesare-herriau@capgemini.com>
2019-10-24 15:26:06 +0200

    Update of the Player documentation.

--- poker_tracker/data/player.py
+++ poker_tracker/data/player.py
@@ -1,6 +1,13 @@
 class Player:
-    """
-    Class Player contains player relative information
+    """ Class Player contains player relative information
+
+        Args:
+            pseudo (string): The pseudo of the player
+
+        Attributes:
+            pseudo (string): The in-game pseudoof the player
+            hands (list): The list of all the hands played by the player
+            games (list): The list of all the games played by the player
     """
     def __init__(self, pseudo):
         """
